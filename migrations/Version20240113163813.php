3cd93f7ac73d8a05705afc2d207b2b4f4c513a51
nicolas <n.brunet.webdev@gmail.com>
2024-01-13 18:12:48 +0100

    ajout auth security

---
+++ migrations/Version20240113163813.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240113163813 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, status VARCHAR(255) NOT NULL, number VARCHAR(255) NOT NULL, delivery TINYINT(1) NOT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, INDEX IDX_F5299398A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE order_detail (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, order_related_id INT NOT NULL, quantity INT NOT NULL, unit_price DOUBLE PRECISION NOT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, UNIQUE INDEX UNIQ_ED896F464584665A (product_id), INDEX IDX_ED896F46F2B83D54 (order_related_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE order_detail ADD CONSTRAINT FK_ED896F464584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('ALTER TABLE order_detail ADD CONSTRAINT FK_ED896F46F2B83D54 FOREIGN KEY (order_related_id) REFERENCES `order` (id)');
+        $this->addSql('ALTER TABLE family ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE product ADD family_id INT DEFAULT NULL, ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL, CHANGE price price DOUBLE PRECISION NOT NULL');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04ADC35E566A FOREIGN KEY (family_id) REFERENCES family (id)');
+        $this->addSql('CREATE INDEX IDX_D34A04ADC35E566A ON product (family_id)');
+        $this->addSql('ALTER TABLE shelf ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD created_at DATETIME DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395');
+        $this->addSql('ALTER TABLE order_detail DROP FOREIGN KEY FK_ED896F464584665A');
+        $this->addSql('ALTER TABLE order_detail DROP FOREIGN KEY FK_ED896F46F2B83D54');
+        $this->addSql('DROP TABLE `order`');
+        $this->addSql('DROP TABLE order_detail');
+        $this->addSql('ALTER TABLE family DROP created_at, DROP updated_at');
+        $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04ADC35E566A');
+        $this->addSql('DROP INDEX IDX_D34A04ADC35E566A ON product');
+        $this->addSql('ALTER TABLE product DROP family_id, DROP created_at, DROP updated_at, CHANGE price price INT NOT NULL');
+        $this->addSql('ALTER TABLE shelf DROP created_at, DROP updated_at');
+        $this->addSql('ALTER TABLE user DROP created_at, DROP updated_at');
+    }
+}
