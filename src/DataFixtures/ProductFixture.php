900d03f8fe9bbe6851437eb73bad4aa52f86d192
nicolas <n.brunet.webdev@gmail.com>
2024-01-17 13:48:50 +0100

    products fixture

--- src/DataFixtures/ProductFixture.php
+++ src/DataFixtures/ProductFixture.php
@@ -7,18 +7,27 @@ use Doctrine\Persistence\ObjectManager;
 use App\Entity\Product;
 use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
 use Faker\Factory;
+use App\Entity\Family;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 ;
 #[HasLifecycleCallbacks()]
-class ProductFixture extends Fixture
+class ProductFixture extends Fixture implements FixtureGroupInterface
 {
     public function load(ObjectManager $manager): void
     {
+        $families = $manager->getRepository(Family::class)->findAll();
+
         $faker = Factory::create('fr_FR');
 
-        for ( $i = 0; $i <5; $i++ ) {
+        for ( $i = 0; $i < 50; $i++ ) {
             $product = new Product();
             $product->setName($faker->word);
             $product->setDescription($faker->sentence);
+            $product->setDescription($faker->sentence);
+            $product->setStock(100);
+            $product->setPrice($faker->randomFloat(1, 5, 30));
+            $product->setImage("dummy_250x250.png");
+            $product->setFamily($families[0]);
             $manager->persist($product);
         }
         $manager->flush();
@@ -31,6 +40,6 @@ class ProductFixture extends Fixture
      */
     public static function getGroups(): array
     {
-        return ['user'];
+        return ['products'];
     }
 }
